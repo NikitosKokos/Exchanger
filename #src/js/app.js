4219d0c3def2a24c763057c2ff94f5fa05ed8c21
Nikita Tsykunov <nikitaodessafreelance@gmail.com>
2021-03-28 08:33:08 +0300

    added pages choice and completion

--- #src/js/app.js
+++ #src/js/app.js
@@ -4,7 +4,7 @@
 @@include("files/some.js", {});
 // @ @include('files/burger.js', {});
 // @ @include("files/spoller.js",{});
-// @ @include("files/select.js",{});
+@@include("files/select.js",{});
 // @ @include("files/tabs.js",{});
 // @ @include("files/sliders.js",{});
     
--- Furgon/js/app.js
+++ Furgon/js/app.js
@@ -647,7 +647,41 @@ ibg();
 ;
 // @ @include('files/burger.js', {});
 // @ @include("files/spoller.js",{});
-// @ @include("files/select.js",{});
+const selectSingle = document.querySelectorAll('.__select');
+const selectSingle_title = document.querySelectorAll('.__select__title');
+
+
+if(selectSingle){
+selectSingle.forEach((element,index) => {
+        // Toggle menu
+        selectSingle_title[index].addEventListener('click', () => {
+        if ('active' === element.getAttribute('data-state')) {
+            element.setAttribute('data-state', '');
+        } else {
+            element.setAttribute('data-state', 'active');
+        }
+        }); 
+        let selectSingle_labels = element.querySelectorAll('.__select__label');
+        // Close when click to option
+        for (let i = 0; i < selectSingle_labels.length; i++) {
+        selectSingle_labels[i].addEventListener('click', (evt) => {
+            selectSingle_title[index].textContent = evt.target.textContent;
+            element.setAttribute('data-state', '');
+            let selectImage = selectSingle_labels[i].querySelector('img').getAttribute('src');
+            let isImg = selectSingle_title[index].querySelector('img');
+                if(isImg){
+                    isImg.remove();
+                }
+            if(selectImage){
+                let imgTitle = document.createElement('img');
+                imgTitle.setAttribute('src',selectImage);
+                selectSingle_title[index].prepend(imgTitle);
+            }
+        });
+        }
+    });
+}
+;
 // @ @include("files/tabs.js",{});
 // @ @include("files/sliders.js",{});
     
